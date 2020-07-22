45b480e7d7e7059b79d429b9b286fa76059b73f5
Tharindu <htharindum1988@gmail.com>
2020-07-22 12:32:23 +0530

    Initial commit

---
+++ AppInitializer.java
@@ -0,0 +1,8 @@
+ public class AppInitializer {
+        public static void main(String args[]){
+            Student s1 = new Student(1,"Tharindu");
+            Syestem.out.println(s1);
+            Student s2 = new Student(2,"Dilini");
+            Syestem.out.println(s2);
+        }
+}
---
+++ Student.java
@@ -0,0 +1,25 @@
+public Student(int id,String name){
+                this.id=id;
+                this.name=name;
+
+                public void setId(int id) {
+                    this.id = id;
+                }
+
+                public String getName() {
+                    return name;
+                }
+
+                public void setName(String name) {
+                    this.name = name;
+                }
+
+                @Override
+                public String toString() {
+                    return "Student{" +
+                            "id=" + id +
+                            ", name='" + name + '\'' +
+                            '}';
+
+                }
+}
